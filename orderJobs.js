64566c3439e57ac2512f60128964be681f686f67
Seth Bell <seth@Seths-MacBook-Pro.local>
2018-08-12 12:18:33 +0100

    Passes tests for inputs with multiple dependencies

--- orderJobs.js
+++ orderJobs.js
@@ -7,16 +7,20 @@ exports.orderJobs = jobData => {
   for (let childJob in jobs) {
     const parentJob = jobs[childJob];
 
+    const parentResultIndex = result.indexOf(parentJob);
+    const childResultIndex = result.indexOf(childJob);
+
     //If no parent dependencies & job not present in result add to front
-    if (!parentJob && !result.includes(childJob)) result.unshift(childJob);
+    if (!parentJob && childResultIndex === -1) result.unshift(childJob);
     //If neither in result add both to end in dependency order
-    else if (!result.includes(childJob) && !result.includes(parentJob))
+    else if (parentResultIndex === -1 && childResultIndex === -1)
       result.push(parentJob, childJob);
     //If parent already in result add child after it
-    else if (result.includes(parentJob)) {
-      const parentResultIndex = result.indexOf(parentJob);
+    else if (parentResultIndex !== -1)
       result.splice(parentResultIndex + 1, 0, childJob);
-    }
+    //If child already in result add any non-empty parent before it
+    else if (childResultIndex !== -1 && parentJob)
+      result.splice(childResultIndex, 0, parentJob);
   }
 
   return result;

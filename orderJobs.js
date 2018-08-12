6f6568a180247355ae825572b236d478891ca36a
Seth Bell <seth@Seths-MacBook-Pro.local>
2018-08-12 11:48:41 +0100

    Passes tests tests for inputs with multiple entries and inputs with one dependency

--- orderJobs.js
+++ orderJobs.js
@@ -4,8 +4,19 @@ exports.orderJobs = jobData => {
 
   if (Object.keys(jobs).length === 0) return result;
 
-  for (let key in jobs) {
-    result.unshift(key);
+  for (let childJob in jobs) {
+    const parentJob = jobs[childJob];
+
+    //If no parent dependencies & job not present in result add to front
+    if (!parentJob && !result.includes(childJob)) result.unshift(childJob);
+    //If neither in result add both to end in dependency order
+    else if (!result.includes(childJob) && !result.includes(parentJob))
+      result.push(parentJob, childJob);
+    //If parent already in result add child after it
+    else if (result.includes(parentJob)) {
+      const parentResultIndex = result.indexOf(parentJob);
+      result.splice(parentResultIndex + 1, 0, childJob);
+    }
   }
 
   return result;

0eaca2f40eb1561f14a57a4343844d2e8dd996e2
Seth Bell <seth@Seths-MacBook-Pro.local>
2018-08-12 10:09:27 +0100

    Passes test for single job

--- orderJobs.js
+++ orderJobs.js
@@ -3,4 +3,10 @@ exports.orderJobs = jobData => {
   const result = [];
 
   if (Object.keys(jobs).length === 0) return result;
+
+  for (let key in jobs) {
+    result.unshift(key);
+  }
+
+  return result;
 };

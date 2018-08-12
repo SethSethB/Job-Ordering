a493de2f13fad3cdaef0f02eb11fa8a9a0254313
Seth Bell <seth@Seths-MacBook-Pro.local>
2018-08-12 11:47:28 +0100

    test for inputs with one dependency

--- spec/orderJobs.spec.js
+++ spec/orderJobs.spec.js
@@ -17,7 +17,22 @@ describe("orderJobs", () => {
   it("An input of jobs with no dependencies will return an array containing all job values in any order", () => {
     const input = '{"a" : "", "b" : "", "c" : ""}';
     const result = orderJobs(input);
-
     expect(result).to.have.members(["a", "b", "c"]);
+  });
+
+  it("Inputs with one dependency return an array containing all job values, with parent job index lower than the dependent child job index", () => {
+    let input = '{"a" : "", "b" : "c", "c" : ""}';
+    let result = orderJobs(input);
+    expect(result).to.have.members(["a", "b", "c"]);
+    let cIndex = result.indexOf("c");
+    let bIndex = result.indexOf("b");
+    expect(cIndex).to.be.lessThan(bIndex);
+
+    input = '{"a" : "", "b" : "", "c" : "a"}';
+    result = orderJobs(input);
+    expect(result).to.have.members(["a", "b", "c"]);
+    aIndex = result.indexOf("a");
+    cIndex = result.indexOf("c");
+    expect(aIndex).to.be.lessThan(cIndex);
   });
 });

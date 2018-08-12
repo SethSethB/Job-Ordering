65b907543ed2553f5f51e617f256947985dabf98
Seth Bell <seth@Seths-MacBook-Pro.local>
2018-08-12 10:01:25 +0100

    Test for simplest input of empty JSON object

--- spec/orderJobs.spec.js
+++ spec/orderJobs.spec.js
@@ -1,4 +1,10 @@
 const { orderJobs } = require("../orderJobs");
 const { expect } = require("chai");
 
-describe("orderJobs", () => {});
+describe("orderJobs", () => {
+  it("An input of an empty JSON object returns an empty array", () => {
+    const input = "{}";
+    const result = orderJobs(input);
+    expect(result).to.eql([]);
+  });
+});

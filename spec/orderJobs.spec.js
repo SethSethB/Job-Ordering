8ad621295c1b7d027a421f27cfc56fbe8973b073
Seth Bell <seth@Seths-MacBook-Pro.local>
2018-08-13 11:37:47 +0100

    Added test for input of empty string

--- spec/orderJobs.spec.js
+++ spec/orderJobs.spec.js
@@ -2,6 +2,12 @@ const { orderJobs } = require("../orderJobs");
 const { expect } = require("chai");
 
 describe("orderJobs", () => {
+  it("An input of an empty string returns an empty array", () => {
+    const input = "";
+    const result = orderJobs(input);
+    expect(result).to.eql([]);
+  });
+
   it("An input of an empty JSON object returns an empty array", () => {
     const input = "{}";
     const result = orderJobs(input);
